a3bc5314d5026e33673a554d398a3046fa73368e
crystal941 <107521386+crystal941@users.noreply.github.com>
2025-03-18 22:38:27 +1300

    Create Logic

--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -4,6 +4,7 @@ public class PipeMoveScript : MonoBehaviour
 {
 
     public float moveSpeed = 5;
+    public float deadZone = -45;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,5 +15,10 @@ public class PipeMoveScript : MonoBehaviour
     void Update()
     {
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+    
+        if (transform.position.x < deadZone){
+            Debug.Log("Pipe Deleted");
+            Destroy(gameObject);
+        }
     }
 }
